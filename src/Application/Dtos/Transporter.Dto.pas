fe5309a5bb34c2b4d61923a2a5ed937bae8732d8
Kennidy-L-Guimaraes <kennidylguimaraes@gmail.com>
2026-04-28 23:40:30 -0300

    feat(Dto): add TCommandResult DTO (Transporter)

--- src/Application/Dtos/Transporter.Dto.pas
+++ src/Application/Dtos/Transporter.Dto.pas
@@ -6,14 +6,28 @@ uses Classes;
    TResultTypes = (crtNone, crtText, crtStream);
   type
    TCommandResult = record
+   public
     Success : Boolean;
     DataType: TResultTypes;
     Text    : string;
     Stream  : TStream;
     Error   : string;
   end;
+  type
+  TStreamResult = class
+    Stream: TStream;
+    destructor Destroy; override;
+  end;
 
 
 implementation
+
+{ TStreamResult }
+
+destructor TStreamResult.Destroy;
+begin
+  Stream.Free;
+  inherited;
+end;
 
 end.
