e4aea81b4543b0c81e3b6f6285e30e3ab42a6577
Kennidy-L-Guimaraes <kennidylguimaraes@gmail.com>
2026-05-27 23:42:35 -0300

    feat(Dto): Add a DTO to assist in parameter passing and centralize the logic of the TAgentController.

---
+++ src/Application/Dtos/Controller.Dto.pas
@@ -0,0 +1,37 @@
+unit Controller.Dto;
+
+interface
+ type
+  TControllerDto = record
+    public
+     {Public Declarations}
+     {Public Declarations}
+      var
+      Screenshot  : string;
+      LiveMode    : string;
+      Messages    : string;
+      Downloads   : string;
+      Shutdown    : string;
+      Registry    : string;
+      Folders     : string;
+      Commands    : string;
+      Information : string;
+      Server      : string;
+      Port        : string;
+      //VAR SYSTEM
+      var
+      name     : string;
+      cpu      : string;
+      ram      : string;
+      Id       : string;
+      ip       : string;
+      status   : string;
+      username : string;
+      version  : string;
+      Receiver : string;
+
+  end;
+
+implementation
+
+end.
